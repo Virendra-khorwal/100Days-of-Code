63f060b8ead59b9d467632c724705f0c9296c554
Virendra-Khorwal <virendrakhorwalvk@gmail.com>
2022-02-28 23:48:42 +0530

    Day 1

--- Day 1/problem4.cpp
+++ Day 1/problem4.cpp
@@ -1,3 +1,5 @@
+// Approach 1
+
 class Solution
 {
 public:
@@ -13,6 +15,28 @@ public:
             }
         }
 
+        return count;
+    }
+};
+
+// Aproach 2
+class Solution
+{
+public:
+    int getDigi(int x)
+    {
+        return log10(x) + 1;
+    }
+    int findNumbers(vector<int> &nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (getDigi(nums[i]) % 2 == 0)
+            {
+                count++;
+            }
+        }
         return count;
     }
 };
